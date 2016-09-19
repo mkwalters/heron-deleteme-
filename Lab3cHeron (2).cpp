4c81febae8f74fc42409f49a13c181b46704b3de
Mitchell <mitchell@shopkeepery>
2016-09-18 22:31:34 -0700

    correct but fails if tri not valid

--- Lab3cHeron (2).cpp
+++ Lab3cHeron (2).cpp
@@ -9,20 +9,32 @@ double triangleArea(double a, double b, double c);
 
 int main()
 {
-	double triangleArea(double a, double b, double c);
+
+	double a, b, c;
+	cout << "Enter the sides of the triangle: " << endl;
+	//I think my cpu didn't like these on the same line. can't say for sure though
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	//probably add some more text here to make things nice
+	cout << triangleArea(a, b, c); // so we only need the return type when we are declaring it
+	// when we go to use a func we just fire it off
+	
 }
 
 
+
+// all this function knows how to do is triangleArea
+// there should not be anything in this func that isn't about getting triangle area
+// that's why all the input was moved to main
 double triangleArea(double a, double b, double c)
 {
 	
-	
-	cout << "Enter the sides of the triangle: " << endl;
-	cin >> a, b, c;
 	double s, area;
 	s = (a+b+c) / 2.0;
 	area = sqrt(s*(s - a)*(s - b)*(s - c));
-	cout << "The area of the triangle is: " << area << endl << endl;
+	return(area);
+	//cout << "The area of the triangle is: " << area << endl << endl;
 	
 }
 
